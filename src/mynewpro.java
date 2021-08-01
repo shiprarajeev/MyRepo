d6724dde91bc3bd3f8fc07cca74cb7ec72e49fba
shiprarajeev <shipracious@gmail.com>
2021-08-01 21:05:11 +0530

    comit

--- src/mynewpro.java
+++ src/mynewpro.java
@@ -1,2 +1,11 @@
-module mynewpro {
+class mynewpro {
+	public static void main(String args[])
+	{
+		System.out.println("rrrrrrrgg");
+	}
+	
+	public static void main1(String args[])
+	{
+		System.out.println("rrrrrrrgg");
+	}
 }
---
+++ src/printa.java
@@ -0,0 +1,7 @@
+
+public class printa {
+	public static void main(String args[])
+	{
+		System.out.println("rrrrrrrgg");
+	}
+}
